34a841039f7df811c3591395b7ee68530ac02859
evanliu23 <eliu23@bu.edu>
2025-10-15 20:40:48 -0400

    update stack creation

--- memos-2/memos-2.s
+++ memos-2/memos-2.s
@@ -6,18 +6,21 @@ _start:
 
     /* Multiboot header -- Safe to place this header in 1st page of memory for GRUB */
     .align 4
-    .long 0x1BADB002 /* Multiboot magic number */
-    .long 0x00000003 /* Align modules to 4KB, req. mem size */
-                      /* See 'info multiboot' for further info */
-    .long 0xE4524FFB /* Checksum */
+    .long 0x1BADB002 // Multiboot magic number 
+    .long 0x00000003 // Align modules to 4KB, req. mem size 
+                      // See 'info multiboot' for further info 
+    .long 0xE4524FFB // Checksum 
 
 real_start:
     /* Setup proper stack */
-    mov %stack_top, %esp
-    mov %esp, %ebp
+    movl %stack_top, %esp
+    movl %esp, %ebp
+
     /* pass arguments to _main */
     pushl %eax
     pushl %ebx
     
     call _main
+   
+    add $8, %esp // restore stack pointer
     hlt
